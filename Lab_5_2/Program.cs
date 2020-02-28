20d2f6c937ba8816b6f7c912be14efe86b0e190e
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-02-27 21:09:41 -0600

    Add files via upload

---
+++ Lab_5_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_5_2
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+
+
+
+            Monday,
+
+            Tuesday,
+
+            Wednesday,
+
+            Thursday,
+
+            Friday,
+
+            Saturday,
+
+            Sunday
+
+        }
+        static void Main(string[] args)
+        {
+            List<string> taskList = new List<string>();
+
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+
+            {
+
+                Console.WriteLine("Enter a new task for " + day);
+
+                string task = day + " : " + Console.ReadLine();
+
+                taskList.Add(task);
+            }
+            foreach (string task in taskList)
+
+            {
+
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
